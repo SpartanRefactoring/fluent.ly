2db86d17ac1aed78a73a2a087089f076e7601bb4
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-30 17:30:10 +0300

    add method

--- src/main/java/an/empty.java
+++ src/main/java/an/empty.java
@@ -2,6 +2,9 @@ package an;
 
 import java.util.*;
 
+import org.jetbrains.annotations.Nullable;
+
+
 /** TODO Yossi Gil: document class
  * @author Yossi Gil
  * @since 2017-04-01 */
@@ -9,5 +12,21 @@ public enum empty {
   ;
   public static <T> List<T> list() {
     return new ArrayList<>();
+  }
+
+  public static <@Nullable T> Iterable<T> iterable() {
+    return new Iterable<T>() {
+      @Override public Iterator<T> iterator() {
+        return new Iterator<T>() {
+          @Override public boolean hasNext() {
+            return false;
+          }
+
+          @Override public T next() {
+            return null;
+          }
+        };
+      }
+    };
   }
 }
