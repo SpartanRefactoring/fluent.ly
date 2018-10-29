306b8a580489a737f5d69112df581bcdb859af29
Yossi Gil <yogi@cs.technion.ac.il>
2018-10-29 22:06:59 +0200

    fewer warnings

--- src/main/java/fluent/ly/nil.java
+++ src/main/java/fluent/ly/nil.java
@@ -5,12 +5,12 @@ import java.util.function.*;
 /** TODO Yossi Gil: document class
  * @author Yossi Gil
  * @since 2017-04-10 */
-public interface nil {
+@SuppressWarnings({ "unused", "null" }) public interface nil {
   interface On<T, R> {
     R on(T t);
   }
 
-  @SuppressWarnings("unused") static <T> T forgetting(final Object _1, final Object... _2) {
+  static <T> T forgetting(final Object _1, final Object... _2) {
     return null;
   }
 
@@ -18,15 +18,15 @@ public interface nil {
     return λ -> λ == null ? null : f.apply(λ);
   }
 
-  @SuppressWarnings("unused") static <T> T ignoring(final boolean __) {
+  static <T> T ignoring(final boolean __) {
     return null;
   }
 
-  @SuppressWarnings("unused") static <T> T ignoring(final double __) {
+  static <T> T ignoring(final double __) {
     return null;
   }
 
-  @SuppressWarnings("unused") static <T> T ignoring(final long __) {
+  static <T> T ignoring(final long __) {
     return null;
   }
 }
